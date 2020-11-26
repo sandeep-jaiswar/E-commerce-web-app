464c4e7c9a3bfd48c14e19d6f41d07629dcb9c3c
sandeep-jaiswar <jaiswarsandeep9@gmail.com>
2020-11-26 14:32:43 -0800

    signin-signup

--- mart/src/App.js
+++ mart/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { useState } from 'react';
 
 function App() {
+  const [viewLogin,setViewLogin]=useState(true);
+  function callview(flag){
+    setViewLogin(flag);
+  }
+
+  function signupClicked(){
+
+  }
+
   return (
-    <div className="App">
-      hi
+    <div className="container-fluid">
+        {viewLogin && <Login callview={callview} viewLogin={viewLogin}></Login>}
+        {!viewLogin && <Signup callview={callview} signupClicked={signupClicked} viewLogin={viewLogin}></Signup>}
     </div>
   );
 }
---
+++ mart/src/components/Login.jsx
@@ -0,0 +1,32 @@
+function Login(props){
+    return (
+        <div className="row" >
+            <div style={{height:"100vh"}}  className="green col-sm-4">
+                <div style={{paddingTop:"53%"}}>
+                <h4>Hello Friend</h4><br/>
+                <h6>Enter your personal Info</h6><br/>
+                <h6>to start your journey with us</h6><br/>
+                <button className="signinupbtn" onClick={()=>props.callview(false)} type="button"> Sign Up </button>
+                </div>
+            </div>
+            <div style={{height:"100vh"}} className="white col-sm-8">
+                <div style={{paddingTop:"20%"}}>
+                    <h3>Sign Into Mart</h3><br/>
+                    <div>
+                    <i className="fal fa-circle"></i>
+                    <i className="fal fa-circle"></i>
+                    <i className="fal fa-circle"></i>
+                    </div><br/>
+                    {/* <h6>or use your email for regiistration</h6> */}
+                    <form onSubmit={props.signupClicked}>
+                        <input placeholder="name"/><br/><br/>
+                        <input placeholder="password"/><br/><br/>
+                        <button className="frgtpswdbtn">forgot password?</button><br/><br/>
+                        <button className="logincrt" type="submit">Sign In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Login;
