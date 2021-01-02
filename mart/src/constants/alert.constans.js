917abe9ff7b8b0fc00e54103ee33e14207e01fff
sandeep-jaiswar <jaiswarsandeep9@gmail.com>
2021-01-02 04:49:05 -0800

    reset

--- mart/src/constants/alert.constans.js
+++
@@ -1,6 +0,0 @@
-export const alertConstants = {
-    SUCCESS: 'ALERT_SUCCESS',
-    ERROR: 'ALERT_ERROR',
-    CLEAR: 'ALERT_CLEAR',
-    UNMATCHED_CONFIRMPASS : 'Password and Confirm Password does not match'
-};
--- mart/src/store/Store.jsx
+++
@@ -1,8 +0,0 @@
-import {createStore} from "redux";
-import Reducers from "../reducers";
-
-export default ()=>{
-    const store = createStore(Reducers);
-    return store;
-}
- 
